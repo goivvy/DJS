d2bdaff8c70cbeb9cdc0aa796feaf79d2579ac84
Konstantin G <go@ivvy.ru>
2019-03-08 18:01:24 -0500

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * It is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to sales@goivvy.com so we can send you a copy immediately.
+ *
+ * @component  Goivvy_DJS
+ * @copyright  Copyright (c) 2017 Goivvy.com. (https://www.goivvy.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     Goivvy.com <sales@goivvy.com>
+ */
+
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+ \Magento\Framework\Component\ComponentRegistrar::MODULE,
+ 'Goivvy_DJS',
+ __DIR__
+);
